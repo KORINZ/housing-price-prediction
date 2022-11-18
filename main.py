bdebf5b4ce7c94d74e715f52094df8fc0a98dc5f
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-18 21:45:32 +0900

    Create training and test sets and stratified them

--- main.py
+++ main.py
@@ -23,13 +23,28 @@ def load_housing_data():
 
 housing = load_housing_data()
 
-housing.hist(bins=50, figsize=(12, 8))
+figure_1 = housing.hist(bins=50, figsize=(12, 8))
 FigureManagement.save_fig("attribute_histogram_plots")
 
 # generate descriptive statistics of the data
 # print(housing.describe().to_string())
 
+# stratify sampling sets
+housing["income_cat"] = pd.cut(housing["median_income"], bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+figure_2 = plt.figure()
+housing["income_cat"].value_counts().sort_index().plot.bar(rot=0, grid=True)
+plt.xlabel("Income Category")
+plt.ylabel("Number of Districts")
+
 # create training and test sets
-train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+strat_train_set, strat_test_set = train_test_split(housing, test_size=0.2, stratify=housing["income_cat"],
+                                                   random_state=42)
+
+# dropping income_cat column since it won't be used again
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+
 
 plt.show()
