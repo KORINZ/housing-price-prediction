78c85733cd55d9726abc655519edcbaf7846080f
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-18 22:32:25 +0900

    Add median_house_value_scatter_plot

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import numpy as np
-import pandas as pd
 import tarfile
 from pathlib import Path
 import urllib.request
 from figure_management import FigureManagement
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
-np.random.seed(42)
 
 
 def load_housing_data():
@@ -32,7 +30,7 @@ FigureManagement.save_fig("attribute_histogram_plots")
 # stratify sampling sets
 housing["income_cat"] = pd.cut(housing["median_income"], bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
                                labels=[1, 2, 3, 4, 5])
-figure_2 = plt.figure()
+figure_2 = plt.figure(2)
 housing["income_cat"].value_counts().sort_index().plot.bar(rot=0, grid=True)
 plt.xlabel("Income Category")
 plt.ylabel("Number of Districts")
@@ -45,6 +43,11 @@ strat_train_set, strat_test_set = train_test_split(housing, test_size=0.2, strat
 for set_ in (strat_train_set, strat_test_set):
     set_.drop("income_cat", axis=1, inplace=True)
 
-
+# exploring training set
+housing = strat_train_set.copy()
+figure_3 = housing.plot(kind="scatter", x="longitude", y="latitude", grid=True, s=housing["population"] / 100,
+                        label="population", c="median_house_value", cmap="jet", colorbar=True, legend=True,
+                        sharex=False, figsize=(10, 7))  # geographical scatter plot
+FigureManagement.save_fig("median_house_value_scatter_plot")
 
 plt.show()
