50806c3ad62ef40247147d958f3c25f8146436eb
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-20 19:02:36 +0900

    Add scaling data and model

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from ClusterSimilarity import ClusterSimilarity
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.compose import make_column_selector, make_column_transformer
+from sklearn.metrics import mean_squared_error
 
 
 def load_housing_data():
@@ -67,5 +73,57 @@ imputer.fit(housing_num)
 X = imputer.transform(housing_num)
 housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=housing_num.index)
 
+# Revert the categorical attribute to numerical
+housing_cat = housing[["ocean_proximity"]]
+cat_encoder = OneHotEncoder()
+housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+
+# Scaling data
+# Gaussian RBF similarity to the nearest cluster center
+cluster_simil = ClusterSimilarity(n_clusters=10, gamma=1., random_state=42)
+similarities = cluster_simil.fit_transform(housing[["latitude", "longitude"]], sample_weight=housing_labels)
+housing_renamed = housing.rename(columns={
+    "latitude": "Latitude", "longitude": "Longitude",
+    "population": "Population",
+    "median_house_value": "Median house value (ᴜsᴅ)"})
+housing_renamed["Max cluster similarity"] = similarities.max(axis=1)
+
+housing_renamed.plot(kind="scatter", x="Longitude", y="Latitude", grid=True,
+                     s=housing_renamed["Population"] / 100, label="Population",
+                     c="Max cluster similarity",
+                     cmap="jet", colorbar=True,
+                     legend=True, sharex=False, figsize=(10, 7))
+plt.plot(cluster_simil.kmeans_.cluster_centers_[:, 1],
+         cluster_simil.kmeans_.cluster_centers_[:, 0],
+         linestyle="", color="black", marker="X", markersize=20,
+         label="Cluster centers")
+plt.legend(loc="upper right")
+FigureManagement().save_fig("district_cluster_plot")
+
+# training the model
+num_pipeline = Pipeline([
+    ("impute", SimpleImputer(strategy="median")),
+    ("standardize", StandardScaler()),
+])
+
+cat_pipeline = make_pipeline(
+    SimpleImputer(strategy="most_frequent"),
+    OneHotEncoder(handle_unknown="ignore"))
+
+preprocessing = make_column_transformer(
+    (num_pipeline, make_column_selector(dtype_include=np.number)),
+    (cat_pipeline, make_column_selector(dtype_include=object)),
+)
+lin_reg = make_pipeline(preprocessing, LinearRegression())
+lin_reg.fit(housing, housing_labels)
+
+housing_prepared = preprocessing.fit_transform(housing)
+
+lin_reg = make_pipeline(preprocessing, LinearRegression())
+lin_reg.fit(housing, housing_labels)
+housing_predictions = lin_reg.predict(housing)
+lin_rmse = mean_squared_error(housing_labels, housing_predictions,
+                              squared=False)
+print(lin_rmse)
 
 plt.show()
